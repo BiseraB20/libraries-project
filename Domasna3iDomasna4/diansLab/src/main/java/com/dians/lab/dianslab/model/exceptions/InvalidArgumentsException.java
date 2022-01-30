964e07fff16301884d3badf75d4218d8423c4c4a
Marko Andonov <marko.20112000@gmail.com>
2022-01-30 12:41:59 +0100

    delete diansLab

--- Domasna3iDomasna4/diansLab/src/main/java/com/dians/lab/dianslab/model/exceptions/InvalidArgumentsException.java
+++
@@ -1,7 +0,0 @@
-package com.dians.lab.dianslab.model.exceptions;
-
-public class InvalidArgumentsException extends RuntimeException{
-    public InvalidArgumentsException() {
-        super("Invalid arguments exception");
-    }
-}
