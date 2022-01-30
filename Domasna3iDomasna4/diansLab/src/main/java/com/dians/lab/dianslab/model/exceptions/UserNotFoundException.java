7238565942d0c27bf670cfeb037025129def6364
Marko Andonov <marko.20112000@gmail.com>
2022-01-30 11:51:51 +0100

    diansLab

--- Domasna3iDomasna4/diansLab/src/main/java/com/dians/lab/dianslab/model/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dians.lab.dianslab.model.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String username) {
-        super(String.format("Username with id %s was not found",username));
-    }
-}
